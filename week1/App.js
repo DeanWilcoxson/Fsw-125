4afce0048af513b671b338d38ff8ea51938a0639
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-02-16 22:06:09 -0800

    2/16/21

---
+++ week1/App.js
@@ -0,0 +1,13 @@
+const {add, sub, mul, div} =  require('./Math');
+
+addResult = add(10,5)
+console.log(addResult)
+
+subResult = sub(10,5) 
+console.log(subResult)
+
+mulResult = mul(10,5)
+console.log(mulResult)
+
+divResult = div(10,5)
+console.log(divResult)
