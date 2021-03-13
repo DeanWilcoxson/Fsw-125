927ef65d52ee776df69a7c75f90b9a9251448e5b
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-03-12 19:27:41 -0800

    3/12/21

---
+++ week4/Assignment2/routes/router.js
@@ -0,0 +1,34 @@
+const { Router } = require("express");
+const express = require("express");
+const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+
+let todos = [{ title: "Trash", description: "Take out the trash", completed: false, id: uuidv4() }];
+
+router.get("/", (req, res) => {
+  res.send(todos);
+});
+
+router.post("/", (req, res) => {
+  const newTodo = req.body;
+  newTodo.id = uuidv4();
+  todos.push(newTodo);
+  res.send(`New object ${req.body.title} added to the database.`);
+});
+
+router.delete("/:todoId", (req, res) => {
+  const todoId = req.params.todoId;
+  const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+  todos.splice(todoIndex, 1);
+  res.send("Item Deleted");
+});
+
+router.put("/:todoId", (req, res) => {
+  const todoId = req.params.todoId;
+  const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+  const updateObject = req.body;
+  const updateTodo = Object.assign(todos[todoIndex], updateObject);
+  res.send(updateTodo);
+});
+
+module.exports = router;
---
+++ week4/Assignment2/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const todoRouter = require('./routes/router')
+
+app.use(express.json());
+app.use("/todo", todoRouter)
+app.listen( 8000, () => {
+    console.log("Server is running on LocalHost:8000")
+} )
